be2baefbe9134337dd54eb89484b530165ffb29f
EasyArch-lyy <1480921056@qq.com>
2020-03-26 16:52:27 +0800

    commit

--- springcloud_user/src/test/schema.sql
+++
@@ -1,3 +0,0 @@
-# drop table user if exists;
-
-create table springcloud.user(id bigint PRIMARY KEY AUTO_INCREMENT,username varchar(40),name varchar(20),age int(3),balance decimal (10,2))CHARSET utf8;
